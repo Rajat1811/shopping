4f9a40685fbe29b2d8bba868829a2c41755aec3a
Rajat1811 <rajatbains73@gmail.com>
2022-05-03 02:10:13 +0530

    first_day

--- Ecommerece/app/views.py
+++ Ecommerece/app/views.py
@@ -1,10 +1,23 @@
+from http.client import NETWORK_AUTHENTICATION_REQUIRED
+from itertools import product
+from unicodedata import category
+from urllib import request
 from django.shortcuts import render
+from django.views import View
+from .models import Customer, Product, Cart, OrderPlaced
 
-def home(request):
- return render(request, 'app/home.html')
+class ProductView(View):
+    def get(self,request):
+        topwears = Product.objects.filter(category='TW')
+        bottomwears = Product.objects.filter(category='BW')
+        mobiles = Product.objects.filter(category='M')
 
-def product_detail(request):
- return render(request, 'app/productdetail.html')
+        return render(request, 'app/home.html', {'topwears': topwears, 'bottomwears': bottomwears, 'mobiles': mobiles})
+
+class ProductDetailView(View):
+    def get(self,request,pk):
+        product= Product.objects.get(pk=pk)
+        return render(request, 'app/productdetail.html', {'product': product})
 
 def add_to_cart(request):
  return render(request, 'app/addtocart.html')
@@ -24,8 +37,16 @@ def orders(request):
 def change_password(request):
  return render(request, 'app/changepassword.html')
 
-def mobile(request):
- return render(request, 'app/mobile.html')
+def mobile(request, data=None):
+    if data==None:
+        mobiles= Product.objects.filter(category='M')
+    elif data== 'iPhone' or data== 'Oppo' or data== 'Nokia':
+        mobiles= Product.objects.filter(category='M').filter(brand=data)
+    elif data == 'below':
+        mobiles=Product.objects.filter(category='M').filter(discounted_price__lt=20000)
+    elif data =='above':
+        mobiles=Product.objects.filter(category='M').filter(discounted_price__gt=20000)
+    return render(request, 'app/mobile.html', {'mobiles': mobiles})
 
 def login(request):
  return render(request, 'app/login.html')
